9760c31c8b11e8fc611972cf9c44db76cbe63275
YangHgRi <yanghgri@gmail.com>
2025-02-27 03:21:40 +0800

    1. 在视频strm化之外新增音频strm化，并且增加下载lrc字幕和webp与gif图片的逻辑

--- app/extensions/exts.py
+++ app/extensions/exts.py
@@ -1,10 +1,12 @@
 from typing import Final
 
 VIDEO_EXTS: Final = frozenset((".mp4", ".mkv", ".flv", ".avi", ".wmv", ".ts", ".rmvb", ".webm", "wmv"))
-EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union((".strm",))
+AUDIO_EXTS: Final = frozenset((".mp3", ".wav"))
 
-SUBTITLE_EXTS: Final = frozenset((".ass", ".srt", ".ssa", ".sub"))
+EXTENDED_VIDEO_EXTS: Final = VIDEO_EXTS.union(AUDIO_EXTS).union((".strm",))
 
-IMAGE_EXTS: Final = frozenset((".png", ".jpg"))
+SUBTITLE_EXTS: Final = frozenset((".ass", ".srt", ".ssa", ".sub", ".lrc"))
+
+IMAGE_EXTS: Final = frozenset((".png", ".jpg", ".webp", ".gif"))
 
 NFO_EXTS: Final = frozenset((".nfo",))
